78e1ae4bc70e7de707b639340082917bb97ed121
Anthony Febus <febus.anthony@gmail.com>
2018-08-11 16:20:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
+

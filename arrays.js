299b4f7492f6cd5cae28ac511a6715206055d7c9
Anthony Febus <febus.anthony@gmail.com>
2018-08-11 16:04:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
 var superheroines = ["catwoman", "she-hulk", "mystique"];
  
 superheroines.push("wonder woman");
+
+var cities = ["New York", "San Francisco"]
+ 
+cities.unshift("Philadelphia")

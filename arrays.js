97d71cf6a06d9eaeb01bf8ef1d38707435beb74d
Anthony Febus <febus.anthony@gmail.com>
2018-08-11 16:04:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ var tomatoSauceIngredients = [
 ];
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
+
+var superheroines = ["catwoman", "she-hulk", "mystique"];
+ 
+superheroines.push("wonder woman");

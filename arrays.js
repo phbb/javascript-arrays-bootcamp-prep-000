a8167a1a8401f152f4b07a6427b2d8a2336abcce
Anthony Febus <febus.anthony@gmail.com>
2018-08-11 16:24:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   return chocolateBars;
 }
  
-function removeElementFromEndOfArray(chocolateBars) {
+function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length - 1);
+  }

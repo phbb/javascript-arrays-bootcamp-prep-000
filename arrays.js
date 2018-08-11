c6cecf6fdcd19bdf23bb5ae3458a990d8a57beba
Anthony Febus <febus.anthony@gmail.com>
2018-08-11 16:14:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,38 +1,6 @@
-var ingredient1 = "bread";
-var ingredient2 = "mild cheese";
-var ingredient3 = "sharp cheese";
-var ingredient4 = "butter";
-var ingredient5 = "tomato";
-var ingredient6 = "garlic";
-
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-];
- 
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-];
-
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 // my TODO task
 
-var superheroines = ["catwoman", "she-hulk", "mystique"];
- 
-superheroines.push("wonder woman");
-// superheroines is now ["catwoman", "she-hulk", "mystique", "wonder woman"]
 
-var cities = ["New York", "San Francisco"];
- 
-cities.unshift("Philadelphia");
-// cities is now ["Philadelphia", "New York", "San Francisco"]
 
 var addElementToBeginningOfArray (array, element)
